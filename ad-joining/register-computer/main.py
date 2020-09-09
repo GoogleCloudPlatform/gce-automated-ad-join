4b304da149da632802850f82d0d86ce2ef8bcc15
Christoph Petersen <cp@peterschen.de>
2020-09-09 17:35:36 +0200

    Fixed function call

--- ad-joining/register-computer/main.py
+++ ad-joining/register-computer/main.py
@@ -540,12 +540,13 @@ def register_computer(request):
         return flask.abort(HTTP_BAD_METHOD)
 
 app = Flask(__name__)
-app.debug=False
+app.debug = False
 
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/cleanup", methods=['GET', 'POST'])
+
 def index():
-    return main.register_computer(request)
+    return register_computer(request)
 
 if __name__ == "__main__":
     app.run()
